ab112a3bd4f0d83a216659fab7d3e98c4c09fadd
Sadikovic Rusmir <95355922+Pablohassan@users.noreply.github.com>
2022-11-01 22:57:17 +0100

    Update VotingTest.js

--- test/VotingTest.js
+++ test/VotingTest.js
@@ -2,11 +2,8 @@ const Voting = artifacts.require("./contracts/Voting.sol");
 const { BN, expectRevert, expectEvent } = require("@openzeppelin/test-helpers");
 const { expect, assert } = require("chai");
 
-//const { beforeEach } = require('eth-gas-reporter/node_modules/mocha');
-//const { describe } = require('eth-gas-reporter/node_modules/mocha');
-
 contract("Voting", (accounts) => {
-  const _owner = accounts[0];
+  const _owner = accounts[0]; //  l'adresse de celui qui deploie le contrat
   const _voter1 = accounts[1];
   const _voter2 = accounts[2];
   const _voter3 = accounts[3];
@@ -14,26 +11,15 @@ contract("Voting", (accounts) => {
 
   const proposal1 = "Tomate";
   const proposal2 = "Peche";
-  const proposal3 = "Pasteque";
 
-  const vote1 = new BN(1);
-  const vote2 = new BN(2);
-  const vote3 = new BN(3);
-
-  const RegisteringVoters = 0;
-  const ProposalsRegistrationStarted = 1;
-  const ProposalsRegistrationEnded = 2;
-  const VotingSessionStarted = 3;
-  const VotingSessionEnded = 4;
-  const VotesTallied = 5;
 
   let VoteInstance;
 
   describe("test functions principales", function () {
     beforeEach(async function () {
+
       VoteInstance = await Voting.new({ fromm: _owner });
-      await VoteInstance.addVoter(_voter1, {from:_owner});
-  
+      await VoteInstance.addVoter(_voter1, { from: _owner });
     });
 
     it("Should register Voters, get event VoterRegistered ", async () => {
@@ -43,14 +29,20 @@ contract("Voting", (accounts) => {
     });
 
     it("Should add voter proposal", async () => {
-    
-    
-      const findEvent = await VoteInstance.addProposal(
-        (proposal1, { from: _voter1 }));
+        
+      await VoteInstance.startProposalsRegistering({ from: _owner });
+      const findEvent = await VoteInstance.addProposal(proposal1, { from: _voter1 });
 
-      await VoteInstance.addProposal (proposal1, { from: _voter1 });
       expectEvent(findEvent, "ProposalRegistered");
     });
+    it("Should setVote ", async () => {
+        await VoteInstance.startProposalsRegistering({ from: _owner });
+        await VoteInstance.endProposalsRegistering({ from: _owner });
+        await VoteInstance.startVotingSession({ from: _owner });
+        const findEvent = await VoteInstance.setVote(0, { from: _voter1, });
+        expectEvent(findEvent, "Voted", );
+      });
+
   });
 
   describe("test addVoter revert", function () {
@@ -80,89 +72,3 @@ contract("Voting", (accounts) => {
     });
   });
 });
-
-// describe("test Voting", function () {
-
-//     beforeEach(async function () {
-//         VoteInstance = await Voting.new({ fromm: owner });
-//         await VoteInstance.addVoter(Voter1);
-//         await VoteInstance.addVoter(Voter2);
-//         VoteInstance = VoteInstance.workflowStatus = 1;
-//         await VoteInstance.addProposal("test", {from:Voter1})
-//         VoteInstance = VoteInstance.workflowStatus = 2;
-//         VoteInstance = VoteInstance.workflowStatus = 3;
-//       });
-
-//       it("should vote, get event Voted "), async () => {
-//         await VoteInstance.setVote(0, { from: Voter1 });
-//          await VoteInstance.voters[Voter1].hasVoted;
-//             expect(VoteInstance.voters[Voter1].hasVoted.to.equal(true));
-//         };
-
-// })
-
-//   describe("test events workflow status", function () {
-//     beforeEach(async function () {
-//       VoteInstance = await Voting.new({ fromm: owner });
-//       await VoteInstance.addVoter(Voter1);
-//       await VoteInstance.addVoter(Voter2);
-//     });
-
-//     it("should check if proposal time is begun", async function () {
-//       await VoteInstance.workflowStatus;
-//       expect((VoteInstance.workflowStatus = 1));
-
-//     });
-//   });
-// });
-
-//     contract('Votin.ProposalsRegistrationEnded admin',
-//     function(accounts){
-//         it("Should autohrise admin to end the proposal session only after it has started",
-//          async function() {
-
-// let VoteInstance = await Voting.deployed();
-// let admin = owner
-
-// try { await VoteInstance.ProposalRegistrantionEnded(
-// {from: Voter});
-// assert.isTrue(false)
-
-// }
-// catch(e) {
-//      assert.isTrue(admin != Voter);
-//      assert.equal(e, "Error: the caller of this function  must be the administrator");
-//            }
-//          });
-//       });
-
-//       contract('SimpleVoting.ProposalRegistrationEnded - successful',
-//       function(accounts) {
-//        it("An account that is not the voting administrator must not be able to end the proposal registration session",
-//          async function() {
-//          //arrange
-//          let VoteInstance = await SimpleVoting.deployed();
-//          let votingAdministrator = await VoteInstance.administrator();
-
-//          await VoteInstance.startProposalsRegistration(
-//             {from: votingAdministrator});
-//          let workflowStatus = await VoteInstance.getWorkflowStatus();
-//          let expectedWorkflowStatus = 1;
-
-//          assert.equal(workflowStatus.valueOf(), expectedWorkflowStatus,
-//             "The current workflow status does not correspond to proposal registration proposal started");
-
-//          //act
-//          await VoteInstance.endProposalsRegistration(
-//            {from: votingAdministrator});
-//          let newWorkflowStatus = await VoteInstance
-//            .getWorkflowStatus();
-//          let newExpectedWorkflowStatus = 2;
-
-//          //assert
-//          assert.equal(newWorkflowStatus.valueOf(), newExpectedWorkflowStatus,
-//            "The current workflow status does not correspond  to proposal registration session ended");
-
-//          });
-//        });
-//   });
